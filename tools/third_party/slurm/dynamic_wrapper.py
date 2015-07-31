754a584458da3c355cec156d1a8f0561245272a6
davidls <davidls@hawaii.edu>
2015-07-30 15:41:28 -1000

    bad format

--- tools/third_party/slurm/dynamic_wrapper.py
+++ tools/third_party/slurm/dynamic_wrapper.py
@@ -16,9 +16,9 @@ def slurm_dynamic_wrapper(app, user):
     native_specs = """-p community.q -N 1 -n 20 --mem=102500 -t 72:00"""
 
     if partition:
-        native_specs = """-p %(partition) -N 1 -n %(max_cpus) --mem=%(max_mem) -t %(max_time)"""%dict(partition=partition, max_cpus=max_cpus, max_mem = int(max_cpus) * int(ram_per_cpu), max_time = max_time)
+        native_specs = """-p %s -N 1 -n %s --mem=%s -t %s"""%(partition, max_cpus, (int(max_cpus) * int(ram_per_cpu)), max_time)
     if account:
-        native_specs = """-A %(account) %(nspec)"""%dict(accont=account, nspec = native_specs)
+        native_specs = """-A %s %s"""%(account, native_specs)
 
     params = dict(submit_native_specification = native_specs, 
                   default_file_action = "none", 
