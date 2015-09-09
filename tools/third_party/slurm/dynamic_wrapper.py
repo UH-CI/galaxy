3d2587b7e01c6c539b2ff1d127f70cdbaf678468
davidls <davidls@hawaii.edu>
2015-09-09 03:10:30 +0000

    corrected a few typos in the dynmic_wrapper

--- tools/third_party/slurm/dynamic_wrapper.py
+++ tools/third_party/slurm/dynamic_wrapper.py
@@ -16,15 +16,15 @@ def slurm_dynamic_wrapper(app, user):
     native_specs = """-p community.q -N 1 -n 20 --mem=102500 -t 72:00"""
 
     if partition:
-        native_specs = """-p %s -N 1 -n %s --mem=%s -t %s"""%(partition, max_cpus, (int(max_cpus) * int(ram_per_cpu)), max_time)
+        native_specs = """-p %(partition)s -N 1 -n %(max_cpus)s --mem=%(max_mem)s -t %(max_time)s"""%dict(partition=partition, max_cpus=max_cpus, max_mem = int(max_cpus) * int(ram_per_cpu), max_time = max_time)
     if account:
-        native_specs = """-A %s %s"""%(account, native_specs)
+        native_specs = """-A %(account)s %(nspec)s"""%dict(accont=account, nspec = native_specs)
 
     params = dict(submit_native_specification = native_specs, 
                   default_file_action = "none", 
                   submit_user = user.username, 
-                  file_action_config = "/lus/scratch/galaxy/config/galaxy/file_action.yml", 
-                  private_token="", 
-                  url = "")
+                  file_action_config = "", 
+                  private_token="",
+                  url = "http://localhost:8913/")
 
     return JobDestination(runner="pulsar", params=params)
